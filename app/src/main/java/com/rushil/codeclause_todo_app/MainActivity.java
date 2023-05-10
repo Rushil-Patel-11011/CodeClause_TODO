6ecabfeed0a3482335e670457171199c823103fe
Rushil Kakadiya <rushilpatel11011@gmail.com>
2023-05-10 19:08:04 +0530

    Created splash screen and added floatingaction button...

--- app/src/main/java/com/rushil/codeclause_todo_app/MainActivity.java
+++ app/src/main/java/com/rushil/codeclause_todo_app/MainActivity.java
@@ -1,11 +1,23 @@
 package com.rushil.codeclause_todo_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ListenerRegistration;
+
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private FloatingActionButton mFab;
+    private FirebaseFirestore firestore;
+//    private ToDoAdapter adapter;
+//    private List<ToDoModel> mList;
+//    private Query query;
+    private ListenerRegistration listenerRegistration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
